7290e0ec64f61a94144ed0d4914a397e881f60cf
sushma rao komirineni <93464466+SushmaRaoChinni@users.noreply.github.com>
2023-04-24 16:55:15 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,17 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import 'normalize.css';
-import './index.css';
-import App from './App';
-import { AppProvider } from './context/appContext';
+import Dashboard from "./Dashboard";
+import Landing from "./Landing";
+import Error from "./Error";
+import Register from "./Register";
 
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <AppProvider>
-    <App />
-    </AppProvider>
-    
-  </React.StrictMode>
-);
+export {Dashboard, Landing, Error, Register}
